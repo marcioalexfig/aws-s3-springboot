a5ff39d3ba2bcbccd0015c08c2183c158736e53e
Márcio Alex <alex.marcio@gmail.com>
2020-07-27 15:08:40 -0300

    arquivos iniciais

---
+++ src/test/java/com/ma/aws/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
